0c9f1c899d94f546e8fb8065e9d980b8952cc5c5
Eric Lluis <ericlluis84@gmail.com>
2024-04-25 08:18:31 +0200

    Add function to create a character

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,138 +1,37 @@
-import type { Fighter, King, Adviser, Squire, Character } from "../types.js";
+import type {
+  Message,
+  Fighter,
+  King,
+  Adviser,
+  Squire,
+  Character,
+  Level,
+} from "../types.js";
 
-export const joffreyBaratheon: King = {
-  name: "Joffrey",
-  lastname: "Baratheon",
-  age: 14,
-  message: "Vais a morir todos",
-  isAlive: true,
-  yearsOfReign: 2,
-  speak(this: King) {
-    return this.message;
-  },
-  die(this: King) {
-    this.isAlive = true;
-  },
-};
+const kingMessage = "Vais a morir todos";
 
-export const jaimeLannister: Fighter = {
-  name: "Jaime",
-  lastname: "Lannister",
-  age: 34,
-  message: "Primero pego y luego pregunto",
-  isAlive: true,
-  weapon: "Sword",
-  skillLevel: 8,
-  speak(this: Fighter) {
-    return this.message;
-  },
-  die(this: Fighter) {
-    this.isAlive = true;
-  },
-};
-
-export const daenerysTargaryen: Fighter = {
-  name: "Daenerys",
-  lastname: "Targaryen",
-  age: 23,
-  message: "Primero pego y luego pregunto",
-  isAlive: true,
-  weapon: "Sword",
-  skillLevel: 9,
-  speak(this: Fighter) {
-    return this.message;
-  },
-  die(this: Fighter) {
-    this.isAlive = true;
-  },
-};
-
-export const tyronLannister: Adviser = {
-  name: "Tyron",
-  lastname: "Lannister",
-  age: 27,
-  message: "No sé por qué, pero creo que voy a morir pronto",
-  isAlive: true,
-  characterWhomItAdvise: daenerysTargaryen,
-  speak(this: Fighter) {
-    return this.message;
-  },
-  die(this: Fighter) {
-    this.isAlive = true;
-  },
-};
-
-export const bronn: Squire = {
-  name: "Bronn",
-  lastname: "",
-  age: 32,
-  isAlive: true,
-  message: "Soy un loser",
-  characterWhomItServes: jaimeLannister,
-  flatteryLevel: 7,
-  speak(this: Squire) {
-    return this.message;
-  },
-  die(this: Squire) {
-    this.isAlive = true;
-  },
-};
-
-export const characters: Character[] = [
-  joffreyBaratheon,
-  jaimeLannister,
-  daenerysTargaryen,
-  tyronLannister,
-  bronn,
-];
-
-const createCharacter = (name: string, lastname: string, age: number) => ({
+const createCharacter = (name: string, lastName: string, age: number) => ({
   name,
-  lastname,
+  lastName,
   age,
   isAlive: true,
 });
 
-const createKingCharacter = (
+export const createKingCharacter = (
+  name: string,
+  lastName: string,
+  age: number,
   yearsOfReign: number,
-  speak: string,
-  die: void,
-) => ({
-  yearsOfReign: 7,
-  speak: "Vais a morir todos",
-  die,
-});
-console.log(createKingCharacter);
-
-const createFighterCharacter = (
-  weapon: string,
-  skillLevel: number,
-  speak: string,
-  die: void,
-) => ({
-  weapon,
-  skillLevel,
-  speak,
-  die,
-});
-
-const createAdviserrCharacter = (
-  characterWhomItAdvise: Fighter | King | Squire,
-  speak: string,
-  die: void,
-) => ({
-  characterWhomItAdvise,
-  speak,
-  die,
-});
-const createSquireCharacter = (
-  characterWhomItServes: Fighter | King | Squire,
-  flatteryLevel: number,
-  speak: string,
-  die: void,
-) => ({
-  characterWhomItServes,
-  flatteryLevel,
-  speak,
-  die,
+): King => ({
+  name,
+  lastName,
+  age,
+  isAlive: true,
+  yearsOfReign,
+  speak(this: King) {
+    return kingMessage;
+  },
+  die(this: King) {
+    this.isAlive = false;
+  },
 });
--- src/types.ts
+++ src/types.ts
@@ -1,11 +1,16 @@
 export type Level = 0 | 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9 | 10;
 
+export type Message =
+  | "Vais a morir todos"
+  | "Primero pego y luego pregunto"
+  | "No sé por qué, pero creo que voy a morir pronto"
+  | "Soy un loser";
+
 export interface Character {
   name: string;
-  lastname: string;
+  lastName: string;
   age: number;
   isAlive: true | false;
-  message: string;
   speak: () => string;
   die: () => void;
 }
