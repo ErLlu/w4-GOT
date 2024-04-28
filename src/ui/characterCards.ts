f8fb289d9ebbd9b38db884c7a545cc543a2c4f0d
Eric Lluis <ericlluis84@gmail.com>
2024-04-28 21:15:21 +0200

    Add characterCards function

---
+++ src/ui/characterCards.ts
@@ -0,0 +1,24 @@
+import { type Character } from "../types.js";
+
+export const createCharacterCard = (character: Character) => {
+  const card = document.createElement("li");
+  const characterTitle = document.createElement("h2");
+
+  const fullName = `${character.name} ${character.lastName}`;
+  characterTitle.textContent = fullName;
+  card.classList.add("character-card");
+
+  const characterAge = document.createElement("p");
+  characterAge.classList.add("character__age");
+  characterAge.textContent = `Age: ${character.age} years`;
+
+  const characterStatus = document.createElement("div");
+  characterStatus.classList.add("character__status");
+  characterStatus.textContent = "Status:";
+
+  card.appendChild(characterTitle);
+  card.appendChild(characterAge);
+  card.appendChild(characterStatus);
+
+  return card;
+};
