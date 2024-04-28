9e993e74c32cce21377866b32db5af93b4570e47
Eric Lluis <ericlluis84@gmail.com>
2024-04-28 21:17:03 +0200

    Add cardContainer function

---
+++ src/ui/cardContainer.ts
@@ -0,0 +1,15 @@
+import { type Character } from "../types";
+import { createCharacterCard } from "./characterCards.js";
+
+export const getCardContainer = (characters: Character[]) => {
+  const charactersList = document.createElement("ul");
+  charactersList.classList.add("card-container");
+
+  characters.forEach((character) => {
+    const characterElement = document.createElement("li");
+    characterElement.appendChild(createCharacterCard(character));
+    charactersList.appendChild(characterElement);
+  });
+
+  return charactersList;
+};
