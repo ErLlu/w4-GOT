d5851a803cefbfaa61a658c745a61c933596ce46
Eric Lluis <ericlluis84@gmail.com>
2024-04-28 22:14:14 +0200

    Add mainContainer

--- src/ui/index.ts
+++ src/ui/index.ts
@@ -1,17 +1,30 @@
 import { getMainContainer } from "./mainContainer.js";
 import { getHeader } from "./header.js";
+import { type Character } from "../types.js";
+import { characters } from "../characters/data/characters.js";
+import { getCardContainer } from "./cardContainer.js";
 
-export const renderGeneralLayout = (webTitle: string): void => {
+export const createMainContainer = () => {
+  const mainContainer = document.createElement("div");
+
+  mainContainer.classList.add("main-container");
+
+  return mainContainer;
+};
+
+export const renderGeneralLayout = (title: string, characters: Character[]) => {
   const rootElement = document.querySelector(".root");
   const mainContainer = getMainContainer();
   const header = getHeader();
+  const caracterList = getCardContainer(characters);
 
   if (!mainContainer) {
     throw new Error("Element not found");
   }
 
   if (rootElement) {
-    mainContainer.appendChild(header);
     rootElement.appendChild(mainContainer);
+    mainContainer.appendChild(header);
+    mainContainer.appendChild(caracterList);
   }
 };
