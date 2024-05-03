6ec15deaf24510a1bcd596d0e6276ccff66cd302
Eric Lluis <ericlluis84@gmail.com>
2024-05-04 01:19:04 +0200

    Modify getHeader test

--- src/ui/header.ts
+++ src/ui/header.ts
@@ -1,12 +1,12 @@
 const root = document.querySelector(".main-container");
 
 export const getHeader = (titleText: string) => {
-  const title = document.createElement("h1");
+  const heading = document.createElement("h1");
   const header = document.createElement("header");
-  title.classList.add("main-title");
+  heading.classList.add("main-title");
 
-  title.textContent = titleText;
-  header.appendChild(title);
+  heading.textContent = titleText;
+  header.appendChild(heading);
 
   return header;
 };
